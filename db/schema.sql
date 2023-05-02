c3dfc107d40b7de5a0327098aed0c8650956b772
JGJ <jordan.gb0@gmail.com>
2023-05-02 01:06:37 -0400

    added employee table

--- db/schema.sql
+++ db/schema.sql
@@ -19,3 +19,18 @@ CREATE TABLE roles (
     ON DELETE SET NULL
 );
 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT REFERENCES employee(id),
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
+);
